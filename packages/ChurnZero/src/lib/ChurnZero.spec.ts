9c4dd03ba4c49f7e045ee872918f5c61959b25a9
Benjamin Charity <ben.charity@hey.com>
2023-01-11 17:40:26 -0500

    temp: fake spec test until specs are written

--- packages/ChurnZero/src/lib/ChurnZero.spec.ts
+++ packages/ChurnZero/src/lib/ChurnZero.spec.ts
@@ -1,4 +1,4 @@
-import {ChurnZero, ChurnZeroPublicAPI} from './ChurnZero';
+import { ChurnZero, ChurnZeroPublicAPI } from './ChurnZero';
 
 const mockFn = jest.fn(() => {});
 
@@ -6,12 +6,11 @@ function generateConfig() {
   return {
     url: 'https://getflowpath-dev.us2app.churnzero.net/',
     apiKey: '1!4p97ShbYcIcmXdAGdudvviZ97ycbazSFQLP8AQZztG4t11DC',
-
-  }
+  };
 }
 function fakeChurnZeroApi() {
   const api = window as Partial<ChurnZeroPublicAPI>;
-  return api.ChurnZero = {
+  return (api.ChurnZero = {
     injectSnippet: mockFn,
     setAppKey: mockFn,
     setModule: mockFn,
@@ -19,17 +18,17 @@ function fakeChurnZeroApi() {
     stop: mockFn,
     toggleUrlTracking: mockFn,
     toggleSilentMode: mockFn,
-    toggleSuccessPanel:mockFn,
+    toggleSuccessPanel: mockFn,
     debug: mockFn,
     push: mockFn,
-    setContact:  mockFn,
-    setAttribute:  mockFn,
-    trackEvent:  mockFn,
+    setContact: mockFn,
+    setAttribute: mockFn,
+    trackEvent: mockFn,
     registerEvent: mockFn,
     deregisterEvent: mockFn,
-  }
+  });
 }
-it('should initiate connection', async() => {
+it('should initiate connection', async () => {
   const config = generateConfig();
   const czscript = document.createElement('script');
   document.body.appendChild(czscript);
@@ -41,6 +40,7 @@ it('should initiate connection', async() => {
   script.onload!(undefined as any);
 
   // await expect(cz).resolves.toBeDefined();
+  await new Promise(process.nextTick);
   expect(api.push).nthCalledWith(1, ['setAppKey', config.apiKey]);
   expect(api.push).toBeCalledTimes(2);
 });
