789aefbab9857e5763166d6ac82fdb94e70451ff
Benjamin Charity <ben.charity@hey.com>
2023-01-12 12:25:45 -0500

    chore: update config fro coverage

--- jest.preset.js
+++ jest.preset.js
@@ -1,3 +1,16 @@
 const nxPreset = require('@nrwl/jest/preset').default;
 
-module.exports = { ...nxPreset, clearMocks: true, coverageReporters: ['text', 'json', 'html'] };
+module.exports = {
+  ...nxPreset,
+  clearMocks: true,
+  collectCoverageFrom: [
+    `**/*.{ts,js}`,
+    `packages/**/*.ts`,
+    `!**/*.stories.ts`,
+    `!**/stories/*`,
+    `!**/testing/**`,
+    `!**/index.ts`,
+    '!**/node_modules/**',
+  ],
+  coverageReporters: ['text', 'json', 'html'],
+};
